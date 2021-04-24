d5254db0fcc2c51ea4f85c50ee3897cfe9ed384e
anhnguyenphung <anhnguyenphung@email.arizona.edu>
2021-04-24 05:43:09 +0100

    Update Controller

--- src/CivController.java
+++ src/CivController.java
@@ -1,4 +1,37 @@
 
 public class CivController {
-
+	CivModel model;
+	
+	public CivController(CivModel model) {
+		this.model = model;
+	}
+	
+	public boolean isGameOver() {
+		int humanUnits = model.getPlayerCurUnits(model.getPlayer("human"));
+		int compUnits = model.getPlayerCurUnits(model.getPlayer("computer"));
+		if (humanUnits == 0 || compUnits == 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public String determineWinner() {
+		int humanUnits = model.getPlayerCurUnits(model.getPlayer("human"));
+		int compUnits = model.getPlayerCurUnits(model.getPlayer("computer"));
+		if (humanUnits > compUnits) {
+			return "Human";
+		} else if (humanUnits < compUnits) {
+			return "Computer";
+		} else {
+			return null;
+		}
+	}
+	
+	public void humanMove(int row, int col) {
+		
+	}
+	
+	public void computerMove() {
+		
+	}
 }
